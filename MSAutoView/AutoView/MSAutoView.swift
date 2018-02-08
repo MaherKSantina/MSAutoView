1a7b8ef0c8475c973a182d96dae98f39cf378bed
MaherKSantina <maher.santina90@gmail.com>
2018-02-08 15:14:44 +1100

    made class open

--- MSAutoView/AutoView/MSAutoView.swift
+++ MSAutoView/AutoView/MSAutoView.swift
@@ -24,7 +24,7 @@
 
 import UIKit
 
-public class MSAutoView: UIView {
+open class MSAutoView: UIView {
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
