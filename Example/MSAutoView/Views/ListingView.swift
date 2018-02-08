f77c6c83b74d2854b2bda64829f2cdd854f08038
Maher Santina <maher.santina90@gmail.com>
2018-02-08 23:44:23 +1100

    Fixed Pods

---
+++ Example/MSAutoView/Views/ListingView.swift
@@ -0,0 +1,50 @@
+/**
+ MIT License
+ 
+ Copyright (c) 2018 MaherKSantina
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+import MSAutoView
+
+class ListingView: MSAutoView {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBInspectable var title: String = "This is a default title"
+    @IBInspectable var details: String = "This is a default details"
+    @IBInspectable var price: Double = 300
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateView()
+    }
+    
+    func updateView() {
+        titleLabel.text = title
+        detailsLabel.text = details
+        priceLabel.text = String(describing: price)
+        
+    }
+    
+}
