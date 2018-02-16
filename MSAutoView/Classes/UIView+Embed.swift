4c57e1161558b7c4f246760e4fa5b56c40a01679
Maher Santina <maher.santina90@gmail.com>
2018-02-16 23:44:08 +1100

    Added class

---
+++ MSAutoView/Classes/UIView+Embed.swift
@@ -0,0 +1,40 @@
+/**
+ MIT License
+ 
+ Copyright (c) 2018 MaherKSantina
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import UIKit
+
+extension UIView{
+    /**
+     Embeds a view inside another view and adds constraints to fit the subview in the whole view
+     - Parameter subview: The subview that will be added
+     */
+    public func addSubviewWithConstraints(_ subview: UIView){
+        self.addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        self.layer.masksToBounds = false
+        
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|", options: NSLayoutFormatOptions.init(rawValue: 0), metrics: nil, views: ["view":subview]))
+        self.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options: NSLayoutFormatOptions.init(rawValue: 0), metrics: nil, views: ["view":subview]))
+    }
+}
